ff7591e5d66dcaa594a35862d6bfc7400b9052b4
Sofia <sofiamino78@gmail.com>
2023-10-08 14:09:37 -0300

    Frogger game

--- WHAC-A-MOLE/app.js
+++ WHAC-A-MOLE/app.js
@@ -43,7 +43,7 @@ function countDown() {
     if (currentTime == 0) {
         clearInterval(countDownTimerId);
         clearInterval(timeId);
-        alert('GAME OVER! Your final score is ' + result);
+        alert('Game Over! Your final score is ' + result);
     }
 }
 
